fe04f37f6a2d9aceadb4056ac828d0fca21e30da
shaban-rayeen <shabanrayeen@gmail.com>
2021-09-03 16:39:51 +0530

    Todo

---
+++ src/main/java/com/todoapp/App.java
@@ -0,0 +1,13 @@
+package com.todoapp;
+
+import com.todoapp.Screens.HomePage;
+
+public class App 
+{
+    // Runner
+
+    public static void main( String[] args )
+    {
+        new HomePage();
+    }
+}
